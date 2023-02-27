cdd3b217b41d4a469494fc5eedf4b7fef78b305d
friaf <shovel.irena@gmail.com>
2023-02-27 14:39:22 +0000

    sql innitial

---
+++ sql-psycopg2.py
@@ -0,0 +1,11 @@
+import psycopg2
+
+# connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# fetch the relults (multiple)
+results = cursor.fetchall()
+
